1ba355d893a55befd68170aee0a1838e2d3b6da3
Orion Antonucci <orion.antonucci@valtech.com>
2024-11-14 10:46:27 -0300

    feat: create Footer cmp

---
+++ src/components/molecules/Footer/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Footer">
+    <p class="TextGray --DarkMode">Photography</p>
+    <p class="TextGray --DarkMode">Copyright</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+
+.Footer {
+    display: flex;
+    background-color: #000;
+    color: #fff;
+    height: 171px;
+    flex-direction: column;
+    padding: 0 50px;
+  }
+@media (min-width: 850px) {
+  .Footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 160px;
+  }
+}
+</style>
