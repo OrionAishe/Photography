68f21e8957e4cdb7e413e0ec094fe5aef9d19602
Orion Antonucci <orion.antonucci@valtech.com>
2025-02-05 14:28:45 -0300

    feat: create portifolio page

---
+++ pages/Portifolio/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <MoleculesHeader></MoleculesHeader>
+  <div>
+    <OrganismsCardDisplay
+      :-cards="cards"
+      Variant="Primary"
+    ></OrganismsCardDisplay>
+  </div>
+  <MoleculesFooter></MoleculesFooter>
+</template>
+
+<script lang="ts" setup>
+import { getEntry } from "~/src/api/contentful";
+const relatedWorks = await getEntry("relatedWorks", "portifolio");
+
+const cards =
+  relatedWorks.data.relatedWorksCollection.items[0].pageCollection.items.map(
+    (card: { title: any; tag: any; mainImage: { media: { url: any } } }) => {
+      return {
+        title: card.title,
+        Tag: card.tag,
+        BGImage: card.mainImage.media.url,
+        link: `/${card.title}`,
+      };
+    }
+  );
+</script>
